70d485dd08776516c08198f3f46dfd3be8665f50
Jen-Mei Wu <jenmei@oaktownlabs.com>
2018-11-29 19:46:38 -0800

    reformat

--- auth/auth.go
+++ auth/auth.go
@@ -14,48 +14,49 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 // [START gmail_quickstart]
 
 package auth
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"os"
-	"errors"
+  "encoding/json"
+  "errors"
+  "fmt"
+  "io/ioutil"
+  "net/http"
+  "os"
 
-	"golang.org/x/net/context"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
-	"google.golang.org/api/gmail/v1"
+  "golang.org/x/net/context"
+  "golang.org/x/oauth2"
+  "golang.org/x/oauth2/google"
+  "google.golang.org/api/gmail/v1"
 )
 
 // Retrieve a token, saves the token, then returns the generated client.
 func getClient(config *oauth2.Config, ctx context.Context) *http.Client {
-	// The file token.json stores the user's access and refresh tokens, and is
-	// created automatically when the authorization flow completes for the first
-	// time.
-	tokFile := "oauth_token.json"
-	tok, err := tokenFromFile(tokFile)
-	if err != nil {
-		tok = getTokenFromWeb(config)
-		saveToken(tokFile, tok)
-	}
-	return config.Client(ctx, tok)
+  // The file token.json stores the user's access and refresh tokens, and is
+  // created automatically when the authorization flow completes for the first
+  // time.
+  tokFile := "oauth_token.json"
+  tok, err := tokenFromFile(tokFile)
+  if err != nil {
+    tok = getTokenFromWeb(config)
+    saveToken(tokFile, tok)
+  }
+  return config.Client(ctx, tok)
 }
 
-func bail (s string, err interface{}) error {
-	msg := fmt.Sprint(s, err)
+func bail(s string, err interface{}) error {
+  msg := fmt.Sprint(s, err)
   panic(errors.New(msg))
 }
 
 // Request a token from the web, then returns the retrieved token.
 func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
-	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+  authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
 
-	fmt.Println("")
+  fmt.Println("")
   fmt.Println("====> Get ready to authenticate....")
   fmt.Println("")
   fmt.Printf("Open the link below in your browser. To give permission to view your email, click 'Allow'"+
@@ -63,58 +64,58 @@ func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
   fmt.Println("")
   fmt.Print("Paste the code here:")
 
-	var authCode string
-	if _, err := fmt.Scan(&authCode); err != nil {
-		bail("Unable to read authorization code: ", err);
-	}
+  var authCode string
+  if _, err := fmt.Scan(&authCode); err != nil {
+    bail("Unable to read authorization code: ", err);
+  }
 
-	tok, err := config.Exchange(context.TODO(), authCode)
-	if err != nil {
-		bail("Unable to retrieve token from web: ", err)
-	}
-	return tok
+  tok, err := config.Exchange(context.TODO(), authCode)
+  if err != nil {
+    bail("Unable to retrieve token from web: ", err)
+  }
+  return tok
 }
 
 // Retrieves a token from a local file.
 func tokenFromFile(file string) (*oauth2.Token, error) {
-	f, err := os.Open(file)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-	tok := &oauth2.Token{}
-	err = json.NewDecoder(f).Decode(tok)
-	return tok, err
+  f, err := os.Open(file)
+  if err != nil {
+    return nil, err
+  }
+  defer f.Close()
+  tok := &oauth2.Token{}
+  err = json.NewDecoder(f).Decode(tok)
+  return tok, err
 }
 
 // Saves a token to a file path.
 func saveToken(path string, token *oauth2.Token) {
-	fmt.Printf("Saving credential file to: %s\n", path)
-	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
-	if err != nil {
-		bail("Unable to cache oauth token: %v", err)
-	}
-	defer f.Close()
-	json.NewEncoder(f).Encode(token)
+  fmt.Printf("Saving credential file to: %s\n", path)
+  f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+  if err != nil {
+    bail("Unable to cache oauth token: %v", err)
+  }
+  defer f.Close()
+  json.NewEncoder(f).Encode(token)
 }
 
 func Client(ctx context.Context) (c *http.Client, err error) {
-	defer func () {
-		if r := recover(); r != nil {
-			err = r.(error)
-		}
-	}()
+  defer func() {
+    if r := recover(); r != nil {
+      err = r.(error)
+    }
+  }()
 
-	b, err := ioutil.ReadFile("client_secret.json")
-	if err != nil {
-		bail("Unable to read client secret file: %v", err)
-	}
+  b, err := ioutil.ReadFile("client_secret.json")
+  if err != nil {
+    bail("Unable to read client secret file: %v", err)
+  }
 
-	// If modifying these scopes, delete your previously saved token.json.
-	config, err := google.ConfigFromJSON(b, gmail.GmailReadonlyScope)
-	if err != nil {
-		bail("Unable to parse client secret file to config: %v", err)
-	}
-	client := getClient(config, ctx)
+  // If modifying these scopes, delete your previously saved token.json.
+  config, err := google.ConfigFromJSON(b, gmail.GmailReadonlyScope)
+  if err != nil {
+    bail("Unable to parse client secret file to config: %v", err)
+  }
+  client := getClient(config, ctx)
   return client, nil
 }
--- gmailservice/gmailservice_test.go
+++ gmailservice/gmailservice_test.go
@@ -1,71 +1,72 @@
 package gmailservice
 
 import (
-	"encoding/json"
-	"google.golang.org/api/gmail/v1"
-	"testing"
-	// "encoding/json"
-	"log"
-	"io/ioutil"
-	"os"
-	"strings"
+  "encoding/json"
+  "google.golang.org/api/gmail/v1"
+  "testing"
+  "io/ioutil"
+  // "encoding/json"
+  "log"
+  "os"
+  "strings"
 )
 
 var expectedBody = "How are you doing? Some special characters: <>?$/:-)"
 
 func getEmailJson() []byte {
-	jsonFile, err := os.Open("gmail_message.json") // Slack notification
+  jsonFile, err := os.Open("gmail_message.json") // Slack notification
 
-	if err != nil {
-		log.Fatalf("Couldn't open gmail_message.json. Error: %v", err)
-	}
-	
-	emailJson, _ := ioutil.ReadAll(jsonFile) // theoretically the same thing that the gmail API returns
-	jsonFile.Close()
-	return emailJson
+  if err != nil {
+    log.Fatalf("Couldn't open gmail_message.json. Error: %v", err)
+  }
+
+  emailJson, _ := ioutil.ReadAll(jsonFile) // theoretically the same thing that the gmail API returns
+  jsonFile.Close()
+  return emailJson
 }
 
 func JsonToGmail(jsonByteArray []byte) (gmail.Message, error) {
-	var data gmail.Message
-	if err := json.Unmarshal(jsonByteArray, &data); err != nil {
-		log.Printf("json.Unmarshal failed, skipping message, err: %v", err)
-		return data, err
-	}
-	return data, nil
+  var data gmail.Message
+  if err := json.Unmarshal(jsonByteArray, &data); err != nil {
+    log.Printf("json.Unmarshal failed, skipping message, err: %v", err)
+    return data, err
+  }
+  return data, nil
 }
 
 func TestDownload(t *testing.T) {
-	t.Skip()
+  t.Skip()
 }
 
 func TestGetIndexOfMessages(t *testing.T) {
-	t.Skip()
+  t.Skip()
 }
 
 func TestDownloadFullMessages(t *testing.T) {
-	t.Skip()
+  t.Skip()
 }
 
 func TestExtractHeader(t *testing.T) {
-	t.Skip()
+  t.Skip()
 }
 
 func TestBodyText(t *testing.T) {
-	emailJson := getEmailJson()
-	gmailMsg, _ := JsonToGmail(emailJson)
+  emailJson := getEmailJson()
+  gmailMsg, _ := JsonToGmail(emailJson)
 
-	if body := BodyText(gmailMsg); !strings.Contains(body, expectedBody) {
-		t.Errorf("Body is incorrect. Should have been:\n\n%v\n\nInstead, got:\n\n%v\n\n", expectedBody, body)	}
+  if body := BodyText(gmailMsg); !strings.Contains(body, expectedBody) {
+    t.Errorf("Body is incorrect. Should have been:\n\n%v\n\nInstead, got:\n\n%v\n\n", expectedBody, body)
+  }
 }
 
 func TestGmailToMessage(t *testing.T) {
-	emailJson := getEmailJson()
-	rawGmail, _ := JsonToGmail(emailJson)
-	msg, err := GmailToMessage(rawGmail, "https://mail.google.com/mail/#inbox")
-	if err != nil {
-		t.Errorf("Unexpected error calling JsonForElasticsearch: %v", err)
-	}
-	if body := msg.Body; !strings.Contains(body, expectedBody) {
-		t.Errorf("Body is incorrect. Should have been:\n\n%v\n\nInstead, got:\n\n%v\n\n", expectedBody, body)
-	}
+  emailJson := getEmailJson()
+  rawGmail, _ := JsonToGmail(emailJson)
+  msg, err := GmailToMessage(rawGmail, "https://mail.google.com/mail/#inbox")
+  if err != nil {
+    t.Errorf("Unexpected error calling JsonForElasticsearch: %v", err)
+  }
+  if body := msg.Body; !strings.Contains(body, expectedBody) {
+    t.Errorf("Body is incorrect. Should have been:\n\n%v\n\nInstead, got:\n\n%v\n\n", expectedBody, body)
+  }
 }
