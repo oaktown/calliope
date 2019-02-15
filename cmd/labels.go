89a6152683a63d0b42083ee48866e3ffe0112b86
Sarah Allen <sarah@ultrasaurus.com>
2019-02-15 15:32:24 -0500

    labels cmd -o shows only user labels (#59)

--- cmd/labels.go
+++ cmd/labels.go
@@ -5,14 +5,16 @@ import (
   "github.com/oaktown/calliope/misc"
   "github.com/oaktown/calliope/store"
   "github.com/spf13/cobra"
+  "log"
 )
 
 var labelName string
+var userLabelsOnly bool
 
 func init() {
   rootCmd.AddCommand(labelsCmd)
   labelsCmd.Flags().StringVarP(&labelName, "label", "l", "", "Look up the label id of a particular label.")
-
+  labelsCmd.Flags().BoolVarP(&userLabelsOnly, "only", "o", false, "Display only user labels.")
 }
 
 var labelsCmd = &cobra.Command{
@@ -21,7 +23,7 @@ var labelsCmd = &cobra.Command{
   Long:  `Get labels and ids from Elasticsearch. Must have downloaded earlier.`,
   Run: func(cmd *cobra.Command, args []string) {
     store := misc.GetStoreClient()
-    switch labelName == ""{
+    switch labelName == "" {
     case false:
       lookupLabel(store)
     default:
@@ -33,18 +35,17 @@ var labelsCmd = &cobra.Command{
 func lookupLabel(store *store.Service) {
   labelId, err := store.FindLabelId(labelName)
   if err != nil {
-    fmt.Printf("Error looking up label %v: %v\n", labelName, err)
+    log.Printf("Error looking up label %v: %v\n", labelName, err)
   }
   fmt.Println(labelId)
 }
 
 func showLabels(store *store.Service) {
-  labels, err := store.GetLabels()
+  labels, err := store.GetLabels(userLabelsOnly)
   if err != nil {
-    fmt.Println("Could not get labels from Elasticsearch. Error: ", err)
+    log.Println("Could not get labels from Elasticsearch. Error: ", err)
   }
   for _, label := range labels {
-
     fmt.Printf("|%30s|%-30s|\n", label.Name, label.Id)
   }
 }
